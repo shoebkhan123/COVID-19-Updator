21845ecf52a60cbdc4443033c771b67ed129e051
Shoeb khan <shoeb.k@netree.in>
2020-04-11 14:11:53 +0530

    Updates

---
+++ precache-manifest.86e99b5e502e7786b560f76be0119452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e1b2b7648029879c82af4608c999e9",
+    "url": "/COVID-19-Updator/index.html"
+  },
+  {
+    "revision": "3f1199b4787318166c6d",
+    "url": "/COVID-19-Updator/static/css/main.6d7b43c6.chunk.css"
+  },
+  {
+    "revision": "ea1a2f9e6270d234710d",
+    "url": "/COVID-19-Updator/static/js/2.4df948b2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Updator/static/js/2.4df948b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1199b4787318166c6d",
+    "url": "/COVID-19-Updator/static/js/main.37e70fd8.chunk.js"
+  },
+  {
+    "revision": "ec0fcc1ac14b82023873",
+    "url": "/COVID-19-Updator/static/js/runtime-main.096b4481.js"
+  },
+  {
+    "revision": "2b91ecb78753206e817c34a45bc9b970",
+    "url": "/COVID-19-Updator/static/media/crona-image.2b91ecb7.png"
+  }
+]);
